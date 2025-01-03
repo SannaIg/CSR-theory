4bfce57d7d471ec8f40afad015cc315b1f8cd9f7
SannaIg <157800029+SannaIg@users.noreply.github.com>
2025-01-03 20:11:24 +0100

    Add files via upload

---
+++ CSR to show.R
@@ -0,0 +1,27 @@
+#CSR green house data
+CSR <- read_csv2("CSR_simple.csv", col_names = T)
+
+#Calculate means
+
+CSR %>% group_by(species) %>% summarise(c=mean(c), 
+                                        s=mean(s),
+
+#Create interactive ternary CSR plot                                                                            r=mean(r)) -> CSR_means
+CSRmeans_plot <- CSR_means %>% 
+  plot_ly() %>% add_trace(type = 'scatterternary',mode = 'markers', 
+                          a = (CSR_means~c),
+                          b = (CSR_means~r),
+                          c = (CSR_means~s),
+                          text = (CSR_means~species),
+                          marker = list( 
+                            symbol = 100,
+                            size = 10,
+                            color= c ("red","blue","green","grey","black","yellow","orange","pink","purple","brown","magenta") 
+                            [as.numeric(as.factor(CSR_means$species))+1],
+                            line = list('width' = 2))) %>%
+  layout(    ternary = list(
+    aaxis = list(title = "C"),
+    baxis = list(title = "R"),
+    caxis = list(title = "S")))
+
+CSRmeans_plot    
